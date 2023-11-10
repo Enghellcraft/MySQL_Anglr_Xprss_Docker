aa263f63d01893ce9f98c18a69cd21036822bafc
Denise <97702569+Enghellcraft@users.noreply.github.com>
2023-11-10 16:28:41 -0300

    added extra info row for rows in table

--- app/src/app/search-results-table/search-results-table.component.ts
+++ app/src/app/search-results-table/search-results-table.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-search-results-table',
   templateUrl: './search-results-table.component.html',
   styleUrls: ['./search-results-table.component.css']
 })
-export class SearchResultsTableComponent implements OnInit {
+export class SearchResultsTableComponent implements OnInit, OnChanges {
   //En este input property se trae la lista de productos desde el app.component principal
   @Input() productos!: any[]
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['productos']) {
+      this.productos = this.productos.map(producto => ({ ...producto, showDetails: false }));
+    }
   }
 
 }
