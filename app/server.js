29bad23eec5cc60401461834699d6c04c5d3d75a
Alejandro Nava <101224369+picsfrunk@users.noreply.github.com>
2023-11-22 06:58:17 -0300

    upd

--- app/server.js
+++ app/server.js
@@ -9,17 +9,11 @@ let app = express();
 
 // MYSQL
 let mysql = require('mysql');
-var connection = mysql.createConnection({
- host : 'mysql-db',
- user : process.env.DB_USER,
- password : process.env.DB_PASS, 
- database : process.env.MYSQL_DATABASE
-})
 
 // CORS
 const cors = require('cors');
 var corsOptions = {
-  origin: '*', // solo permite este origen
+  origin: 'localhost', // solo permite este origen
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // metodos permitidos
   optionsSuccessStatus: 200 // status
  }
@@ -27,14 +21,14 @@ app.use(cors(corsOptions));
 
 // DOTENV
 require('dotenv').config({ debug: true });
-let db = mysql.createConnection({
+let connection = mysql.createConnection({
   host: 'mysql-db',
   user: process.env.DB_USER, // importante crear el usuario y dar privilegios para acceder a mydb
   password: process.env.DB_PASS, // y colocar esta password
-  database: process.env.DB_NAME
+  database: 'mydb'
 });
 
-db.connect(function(err) {
+connection.connect(function(err) {
   if (err) {
     console.error('Error conectando a MySQL: ' + err.stack)
     throw err;
@@ -49,19 +43,19 @@ app.get('/api/productos', (req, res) => {
   // se declara la query principal que recibe todos los productos
   let query = 'SELECT gp.id_producto, p.nombre, g.nombre AS nombreGondola, pres.desc_presentacion, gpr.fecha, gpr.cantidad' +
   ' FROM Gondola_Producto AS gp INNER JOIN producto AS  p on gp.id_producto = p.id_producto' +
-  ' INNER JOIN gondola AS g on gp.id_gondola = g.id_gondola INNER JOIN presentacion AS pres on' + 
+  ' INNER JOIN gondola AS g on gp.id_gondola = g.id_gondola INNER JOIN presentacion AS pres on' +
   ' gp.Presentacion_id_presentacion = pres.id_presentacion INNER JOIN Gondola_Producto_Repositor' +
   ' AS gpr on gp.id_producto = gpr.id_producto WHERE gp.id_gondola = gpr.id_gondola AND gp.id_gondola = gpr.id_gondola'
 
   // en caso de que exista repositorId y/o sectorId en los params se concatenara al query la siguiente sentencia
-  if (repositorId) 
+  if (repositorId)
     query += ` AND gpr.id_repositor = ${repositorId}`;
-  if (sectorId) 
+  if (sectorId)
     query += ` AND g.id_sector = ${sectorId}`;
 
   query += ' ORDER BY gp.id_producto ASC'
-  
-  db.query(query, (err, result) => {
+
+  connection.query(query, (err, result) => {
     if (err) {
       console.error('Error en la consulta SQL: ', err);
       res.status(500).json({ mensaje: 'Error al obtener productos filtrados' });
@@ -74,7 +68,7 @@ app.get('/api/productos', (req, res) => {
 
 app.get('/api/sectores', (req, res) => {
   let sql = 'SELECT id_sector, desc_sector FROM Sector';
-  db.query(sql, (err, results) => {
+  connection.query(sql, (err, results) => {
     if (err) {
       throw err;
     }
@@ -84,7 +78,7 @@ app.get('/api/sectores', (req, res) => {
 
 app.get('/api/repositores', (req, res) => {
   let sql = 'SELECT id_repositor, nombre FROM Repositor';
-  db.query(sql, (err, results) => {
+  connection.query(sql, (err, results) => {
     if (err) {
       throw err;
     }
